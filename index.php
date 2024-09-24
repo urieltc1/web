57cf73286558adefe6ebf65d087406fdabf6c092
urieltc1 <urieltc86@gmail.com>
2024-09-24 13:51:56 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!--boostrap -->
+    <link  rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!--fontawesome -->
+    <link href="assets/css/fontawesome.css" rel="stylesheet">
+    <!--wait Me -->
+    <link  rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+
+    <!-- -->
+
+
+
+
+</head>
+<body>
+  
+<div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+      <div class="container">
+        <a href="../" class="navbar-brand">Bootswatch</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" id="themes">Themes <span class="caret"></span></a>
+              <div class="dropdown-menu" aria-labelledby="themes">
+                <a class="dropdown-item" href="../default/">Default</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="../cerulean/">Cerulean</a>
+                <a class="dropdown-item" href="../cosmo/">Cosmo</a>
+                <a class="dropdown-item" href="../cyborg/">Cyborg</a>
+                <a class="dropdown-item" href="../darkly/">Darkly</a>
+                <a class="dropdown-item" href="../flatly/">Flatly</a>
+                <a class="dropdown-item" href="../journal/">Journal</a>
+                <a class="dropdown-item" href="../litera/">Litera</a>
+                <a class="dropdown-item" href="../lumen/">Lumen</a>
+                <a class="dropdown-item" href="../lux/">Lux</a>
+                <a class="dropdown-item" href="../materia/">Materia</a>
+                <a class="dropdown-item" href="../minty/">Minty</a>
+                <a class="dropdown-item" href="../morph/">Morph</a>
+                <a class="dropdown-item" href="../pulse/">Pulse</a>
+                <a class="dropdown-item" href="../quartz/">Quartz</a>
+                <a class="dropdown-item" href="../sandstone/">Sandstone</a>
+                <a class="dropdown-item" href="../simplex/">Simplex</a>
+                <a class="dropdown-item" href="../sketchy/">Sketchy</a>
+                <a class="dropdown-item" href="../slate/">Slate</a>
+                <a class="dropdown-item" href="../solar/">Solar</a>
+                <a class="dropdown-item" href="../spacelab/">Spacelab</a>
+                <a class="dropdown-item" href="../superhero/">Superhero</a>
+                <a class="dropdown-item" href="../united/">United</a>
+                <a class="dropdown-item" href="../vapor/">Vapor</a>
+                <a class="dropdown-item" href="../yeti/">Yeti</a>
+                <a class="dropdown-item" href="../zephyr/">Zephyr</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../help/">Help</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://blog.bootswatch.com/">Blog</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" id="download">Cosmo <span class="caret"></span></a>
+              <div class="dropdown-menu" aria-labelledby="download">
+                <a class="dropdown-item" rel="noopener" target="_blank" href="https://jsfiddle.net/bootswatch/dsjzv5mf/">Open in JSFiddle</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="../5/cosmo/bootstrap.min.css" download>bootstrap.min.css</a>
+                <a class="dropdown-item" href="../5/cosmo/bootstrap.css" download>bootstrap.css</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="../5/cosmo/_variables.scss" download>_variables.scss</a>
+                <a class="dropdown-item" href="../5/cosmo/_bootswatch.scss" download>_bootswatch.scss</a>
+              </div>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-md-auto">
+            <li class="nav-item">
+              <a target="_blank" rel="noopener" class="nav-link" href="https://github.com/thomaspark/bootswatch/"><i class="fa fa-github"></i> GitHub</a>
+            </li>
+            <li class="nav-item">
+              <a target="_blank" rel="noopener" class="nav-link" href="https://twitter.com/bootswatch"><i class="fa fa-twitter"></i> Twitter</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    
+    </div>
+
+  
+Uriel
+
+
+<!--JQuery de boostrap-->
+    <script src="../_vendor/jquery/dist/jquery.min.js"></script>
+    <script src="../_vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../_vendor/prismjs/prism.js" data-manual></script>
+    <script src="../_assets/js/custom.js"></script>
+    <!--Sweet Alert2-->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>       
+
+</body>
+</html>
